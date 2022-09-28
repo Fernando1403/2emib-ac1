d79cd7af6542720ee2443318b43462495d9e9a39
Fernando <84139372+Fernando1403@users.noreply.github.com>
2022-09-28 12:52:19 -0300

    Delete routes directory

--- routes/IsaacNewton.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app)=>{
-    app.get('/IsaacNewton',(req,res)=>{
-        res.render('IsaacNewton.ejs')
-    })
-}
--- routes/neil.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app)=>{
-    app.get('/neil',(req,res)=>{
-        res.render('Neil.ejs')
-    })
-}
